af6bbbe9699751426ff861bfc700110d68f9695a
munialom <munialom@gmail.com>
2025-10-06 19:21:29 +0300

    bar man module activate

--- src/main/java/com/ctecx/brsuite/BrsuiteApplication.java
+++ src/main/java/com/ctecx/brsuite/BrsuiteApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BrsuiteApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BrsuiteApplication.class, args);
 	}
 
